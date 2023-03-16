29ac2434460beac0c2eda7902885b59c0af1a933
Humberto Alfaro <humbertoalfaro@Humbertos-MacBook-Pro.local>
2023-03-15 23:47:16 -0400

    added feature to include else if all statements = false

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-let lChar= ['a', 'b', 'c', 'd', 'e', 'f'];
-let uChar= ['A', 'B', 'C', 'D', 'E', 'F'];
-let nChar= ['1', '2', '3', '4', '5'];
-let sChar= ['!', '@', '#', '$'];
-let possibleChar = [];
-let finalPass = [];
+let lChar = ['a', 'b', 'c', 'd', 'e', 'f'];
+let uChar = ['A', 'B', 'C', 'D', 'E', 'F'];
+let nChar = ['1', '2', '3', '4', '5'];
+let sChar = ['!', '@', '#', '$'];
+// let possibleChar = [];
+// let finalPass = [];
 
 function generatePassword() {
-  let passLength = prompt('How long would you like your password?(Must be 8-128 char)');
+
+  let possibleChar = [];
+  let finalPass = [];
+  let ifAllfalse=[lChar, uChar, nChar, sChar]
+
+  let passLength = prompt('How long would you like your password? (Must be 8-128 characters)');
+  if (passLength < 8 || passLength > 128) {
+    alert('Must me 8-12 characters')
+    return;
+  }
+
+  if (ifAllfalse===false) {
+    alert('Please choose a password type!')
+  }
 
   let lowercase = confirm('Would you like lowercases?');
-  if(lowercase) {
+  if (lowercase) {
     possibleChar = possibleChar.concat(lChar);
-    
-  };
+
+  }
 
   let uppercase = confirm('Would you like uppercases?');
-  if(uppercase) {
+  if (uppercase) {
     possibleChar = possibleChar.concat(uChar);
   };
 
   let numeric = confirm('Would you like numerics?');
-  if(numeric) {
+  if (numeric) {
     possibleChar = possibleChar.concat(nChar);
   };
 
   let special = confirm('Would you like special characters?');
-  if(special){
+  if (special) {
     possibleChar = possibleChar.concat(sChar);
+  }else if(!lowercase && !uppercase && !numeric){
+    alert('Please choose a password type!')
+    return;
   };
-console.log(possibleChar)
+
+  console.log(possibleChar)
   for (let index = 0; index < passLength; index++) {
-    finalPass.push(possibleChar[Math.floor(Math.random()*possibleChar.length)])
+    finalPass.push(possibleChar[Math.floor(Math.random() * possibleChar.length)])
     console.log(finalPass);
   }
   return finalPass.join('')
